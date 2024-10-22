361f1266bdc66f0e95737a22846f20f4138af157
Nicholas Junker <NJunker@thorlabs.com>
2024-10-22 11:50:05 -0400

    first commit

---
+++ MCP9601_lib.cpp
@@ -0,0 +1,156 @@
+/*
+ * MCP960X_lib.cpp
+ *
+ *  Created on: May 1, 2024
+ *      Author: NJunker
+ */
+
+#include "MCP9601_lib.h"
+
+void Thorlabs_MCP9601::begin(uint8_t addr)
+{
+	_addr = addr;
+
+	Thorlabs_I2C_setup();
+}
+
+void Thorlabs_MCP9601::write_register(uint8_t reg, uint8_t data)
+{
+	uint8_t cmd[2];
+	//build command word
+	cmd[0] = reg;
+	cmd[1] = data;
+	
+	Thorlabs_I2C_begin();
+	Thorlabs_I2C_write(cmd, sizeof(cmd));
+	Thorlabs_I2C_end();
+}
+
+uint8_t Thorlabs_MCP9601::read_register(uint8_t reg)
+{
+	uint8_t cmd[1];
+	uint8_t _read_buf[1];
+	//build command word
+	cmd[0] = reg;
+	
+	Thorlabs_I2C_begin();
+	Thorlabs_I2C_write(cmd, sizeof(cmd));
+	Thorlabs_I2C_end();
+	Thorlabs_I2C_read(_read_buf, sizeof(_read_buf));
+
+	//Data is MSB first
+	return _read_buf[0];
+}
+
+float Thorlabs_MCP9601::read_thermocouple()
+{
+	uint8_t cmd[1];
+	uint8_t _read_buf[2];
+	//build command word
+	cmd[0] = MCP9601_HOTJUNCTION_REG;
+	
+	Thorlabs_I2C_begin();
+	Thorlabs_I2C_write(cmd, sizeof(cmd));
+	Thorlabs_I2C_end();
+	//HAL_I2C_Master_Transmit(&_i2c_instance, (_addr << 1), cmd, sizeof(cmd), 1000);
+	
+
+	Thorlabs_I2C_read(_read_buf, sizeof(_read_buf));
+
+	//Wire.requestFrom(_addr, sizeof(_read_buf));
+	//_read_buf[0] = Wire.read();
+	//_read_buf[1] = Wire.read();
+
+	//word is least significant byte first
+	int16_t reading =  (((uint16_t) _read_buf[0]) << 8 | (uint16_t) _read_buf[1]);
+	float temp = reading * 0.0625; //0.0625 degrees C per LSB in register
+
+	return temp;
+}
+
+float Thorlabs_MCP9601::read_ambient()
+{
+	uint8_t cmd[1];
+	uint8_t _read_buf[2];
+	//build command word
+	cmd[0] = MCP9601_COLDJUNCTION_REG;
+	
+	Thorlabs_I2C_begin();
+	Thorlabs_I2C_write(cmd, sizeof(cmd));
+	Thorlabs_I2C_end();
+	//HAL_I2C_Master_Transmit(&_i2c_instance, (_addr << 1), cmd, sizeof(cmd), 100);
+	
+	Thorlabs_I2C_read(_read_buf, sizeof(_read_buf));
+
+	//Wire.requestFrom(_addr, sizeof(_read_buf));
+	//_read_buf[0] = Wire.read();
+	//_read_buf[1] = Wire.read();
+	//HAL_I2C_Master_Receive(&_i2c_instance, (_addr << 1), _read_buf, sizeof(_read_buf), 100);
+
+	//word is most significant byte first
+	int16_t reading =  (((uint16_t) _read_buf[0]) << 8 | (uint16_t) _read_buf[1]);
+	float temp = reading * 0.0625; //0.0625 degrees C per LSB in register
+
+	return temp;
+}
+
+void Thorlabs_MCP9601::setConfig(thermocouple_types type, filter_coefficient coeff)
+{
+	//bit shift our thermocouple data to fit bits 6-4
+	uint8_t data = ((uint8_t)type << 4);
+
+	//bit-wise OR to add in our filter coefficient
+	data |= (uint8_t)coeff;
+	write_register(MCP9601_SENSOR_CONFIG_REG, data);
+}
+
+void Thorlabs_MCP9601::setResolution(adc_resolution res)
+{
+	//bit shift our thermocouple data to fit bits 6-5
+	uint8_t data = ((uint8_t)res << 5);
+	write_register(MCP9601_DEVICE_CONFIG_REG, data);
+}
+
+uint8_t Thorlabs_MCP9601::getStatus()
+{
+	return read_register(MCP9601_STATUS_REG);
+}
+
+//-----------------------------------------------------------------------
+//------------------- To be implemented by user -------------------------
+//-----------------------(Platform Specific)-----------------------------
+//-----------------------------------------------------------------------
+
+uint8_t Thorlabs_MCP9601::Thorlabs_I2C_write(uint8_t *buf, size_t size) {
+	//Implement this in a parent class or modify for your platform
+
+	//Take in an array of single bytes (buf) and length
+	//Return length of bytes successfully transmitted
+}
+
+void Thorlabs_MCP9601::Thorlabs_I2C_read(uint8_t *buf, size_t size) {
+	//Implement this in a parent class or modify for your platform
+
+	//Takes in a pointer to a buffer to place data in, and size of said buffer
+}
+
+void Thorlabs_MCP9601::Thorlabs_I2C_begin() {
+	//Implement this in a parent class or modify for your platform
+
+	//Used if your platform has an I2C transaction begin function (i.e. Arduino)
+}
+
+void Thorlabs_MCP9601::Thorlabs_I2C_end() {
+	//Implement this in a parent class or modify for your platform
+	
+	//Used if your platform has an I2C transaction end function (i.e. Arduino)
+}
+
+void Thorlabs_MCP9601::Thorlabs_I2C_setup() {
+	//Implement this in a parent class or modify for your platform
+
+	//Platform specific startup code, i.e. setting pins, clock speed, etc
+}
+
+
+
---
+++ MCP9601_lib.h
@@ -0,0 +1,103 @@
+/******************************************************************************
+This I2C driver is used to interface the Microchip MCP9601 Thermocouple chip.
+
+   \date          1-May-2024
+   \copyright     2024 Thorlabs Spectral Works
+   \author       Nicholas Junker (njunker@thorlabs.com)
+
+******************************************************************************/
+
+#ifndef INC_MCP9601_LIB_H_
+#define INC_MCP9601_LIB_H_
+
+//#include <Arduino.h>
+//#include <Wire.h>
+#include <cstdint> //for uint8_t, etc
+#include <cstddef> //for size_t
+
+//Default I2C instance used
+//extern I2C_HandleTypeDef hi2c1;
+
+#define MCP9601_HOTJUNCTION_REG 0x00
+#define MCP9601_JUNCTIONDELTA_REG 0x01
+#define MCP9601_COLDJUNCTION_REG 0x02
+#define MCP9601_RAWDATA_REG 0x03
+#define MCP9601_STATUS_REG 0x04
+#define MCP9601_SENSOR_CONFIG_REG 0x05
+#define MCP9601_DEVICE_CONFIG_REG 0x06
+#define MCP9601_DEVICEID_REG 0x20
+
+class Thorlabs_MCP9601 {
+public:
+	//TODO make some nicer functions
+	typedef enum {
+	  TYPE_K,
+	  TYPE_J,
+	  TYPE_T,
+	  TYPE_N,
+	  TYPE_S,
+	  TYPE_E,
+	  TYPE_B,
+	  TYPE_R
+	} thermocouple_types;
+
+	typedef enum {
+	  FILTER_OFF,
+	  FILTER_1, //minimum filter
+	  FILTER_2,
+	  FILTER_3,
+	  FILTER_4, //mid filter
+	  FILTER_5,
+	  FILTER_6,
+	  FILTER_7 //max filter
+	} filter_coefficient;
+
+	typedef enum {
+	  RES_18_BIT, //320ms conversion
+	  RES_16_BIT, //80ms conversion
+	  RES_14_BIT, //20ms conversion
+	  RES_12_BIT //5ms conversion
+	} adc_resolution;
+
+
+	//Initialize MCP with I2C address.
+	void begin(uint8_t addr = 0x65);
+
+	//Read thermocouple temperature, calculate degrees C
+	float read_thermocouple();
+
+	//Read ambient temperature, calculate degrees C
+	float read_ambient();
+
+	//Set thermocouple type and filter coefficent
+	void setConfig(thermocouple_types type, filter_coefficient coeff);
+
+	//Set internal ADC resolution
+	void setResolution(adc_resolution res);
+
+	//Get status register. Short/open circuit detection, alerts 1-4
+	uint8_t getStatus();
+
+protected:
+
+	uint8_t _addr;
+
+	uint8_t Thorlabs_I2C_write(uint8_t *buf, size_t size) __attribute__((weak));
+
+	void Thorlabs_I2C_read(uint8_t *buf, size_t size) __attribute__((weak));
+
+	void Thorlabs_I2C_begin() __attribute__((weak));
+
+	void Thorlabs_I2C_end() __attribute__((weak));
+
+	void Thorlabs_I2C_setup() __attribute__((weak));
+
+private:
+
+	void write_register(uint8_t reg, uint8_t data);
+	uint8_t read_register(uint8_t reg);
+};
+
+
+
+#endif /* INC_MCP9601_LIB_H_ */
